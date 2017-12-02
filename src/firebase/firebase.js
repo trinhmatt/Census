677fefab7abc9cf83ac77623b4323f8aa7f947c4
trinhmatthew@gmail.com <trinhmatthew@gmail.com>
2017-12-02 11:11:28 -0500

    Set up project for heroku deployment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyBBZFHQ-Us0gRFJGWeZRDddaPoEQZjYD4I",
-    authDomain: "survayyy-7df7c.firebaseapp.com",
-    databaseURL: "https://survayyy-7df7c.firebaseio.com",
-    projectId: "survayyy-7df7c",
-    storageBucket: "survayyy-7df7c.appspot.com",
-    messagingSenderId: "163144342720"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
 firebase.initializeApp(config)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+dotenv.config({path: '.env.development'})
 
 module.exports = (env) => {
   const isProduction = env === 'production'
@@ -36,7 +40,15 @@ module.exports = (env) => {
       }]
     },
     plugins: [
-      cssExtract
+      cssExtract,
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+      })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
